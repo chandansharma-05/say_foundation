a7d7085f7f5409fa0538644a087b5b459b4f6904
Chandan Sharma <chandansharma-05@users.noreply.github.com>
2022-05-07 13:32:22 +0530

    Initial Files

---
+++ precache-manifest.577ffcda52eefca292fd72e48afa98f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3ec290782c8662c1cc6381c84b5426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ab55622f42e6737ae0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b61a54856621b39152a9",
+    "url": "/static/js/2.53558318.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.53558318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ab55622f42e6737ae0",
+    "url": "/static/js/main.efb1e1d2.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
